c441830633353add691ac598c2bb1a47e550c118
Matheus <matheus.brgsz@gmail.com>
2025-04-15 19:42:57 -0300

    restructure routing by separating bookings and users into distinct modules

--- src/scheduler_api/app.py
+++ src/scheduler_api/app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from .routers import router
+from .routers import bookings, users
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(users.router)
+app.include_router(bookings.router)
--- src/scheduler_api/routers/bookings.py
+++ src/scheduler_api/routers/bookings.py
@@ -2,66 +2,14 @@ from datetime import datetime
 from http import HTTPStatus
 
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import or_, select
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from . import schema
-from .database import get_session
-from .models import Availability, Booking, User
+from scheduler_api import schema
+from scheduler_api.database import get_session
+from scheduler_api.models import Availability, Booking, User
 
 router = APIRouter()
-
-
-# Users
-@router.post(
-    path='/users/',
-    status_code=HTTPStatus.CREATED,
-    response_model=schema.UserPublic,
-)
-def create_user(
-    user: schema.UserCreate, session: Session = Depends(get_session)
-):
-    user_db = session.scalar(
-        select(User).where(
-            or_(User.username == user.username, User.email == user.email)
-        )
-    )
-
-    # Validate if user credentials are unique
-    if user_db:
-        if user_db.username == user.username:
-            raise HTTPException(
-                status_code=HTTPStatus.BAD_REQUEST,
-                detail='Username already exists',
-            )
-        if user_db.email == user.email:
-            raise HTTPException(
-                status_code=HTTPStatus.BAD_REQUEST,
-                detail='Email already exists',
-            )
-
-    # User Creation
-    user_db = User(username=user.username, email=user.email)
-    session.add(user_db)
-    session.commit()
-
-    # Availabities Creation
-    for date in user.availability:
-        day = date.day
-
-        for slot in date.slots:
-            start = slot.start
-            end = slot.end
-
-            availability = Availability(
-                user_id=user_db.id, day=day, start=start, end=end
-            )
-            session.add(availability)
-
-    session.commit()
-    session.refresh(user_db)
-
-    return user_db
 
 
 @router.post(
